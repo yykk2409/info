61cf25bb8bea847fc3f347da7815b10b00785afd
yykk2409 <yyimymemine3684@gmail.com>
2025-05-21 16:07:20 +0900

    Update main.js

--- main.js
+++ main.js
@@ -1,8 +1,7 @@
 let counter = 0;
-  const h1 = document.querySelector('h1');
-  h1.addEventListener('click', () => {
-      counter++;
-      if (counter >= 10){
-          window.location.href = 'https://google-zotv.onrender.com';
-      }
-  })
+function countup(){
+    counter++;
+    if (counter >= 10){
+        window.location.href = 'https://google-zotv.onrender.com';
+    }
+}
